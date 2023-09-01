03e1a1603e5a0cdd508517fbc81a4a7d065efcbb
prosperemebo <emeboprosper@gmail.com>
2023-09-01 03:01:22 +0100

    Task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_delete - function that deletes an entire binary tree
+ * @tree: tree
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+		return;
+
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+
+	free(tree);
+}
