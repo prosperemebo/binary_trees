30d7262f4a3025f1708c526e2eaffce1079edab2
prosperemebo <emeboprosper@gmail.com>
2023-09-01 03:22:17 +0100

    Task in order

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_inorder - This function that goes through
+ * a binary tree using in-order traversal
+ * @tree: Input tree
+ * @func: Printing handler
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
+}
