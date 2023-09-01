1c6cada5dba5f1cc9b3fa408f6fbf17528ca62e7
prosperemebo <emeboprosper@gmail.com>
2023-09-01 02:54:36 +0100

    Added betty docs

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,6 +1,13 @@
 #include "binary_trees.h"
 #include "stdlib.h"
 
+/**
+ * binary_tree_insert_left - This Function inserts a node as
+ * the left-child of input node
+ * @parent: Parent
+ * @value: Value
+ * Return: Pointer to the created node
+ */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
