88904078dc0612bc77ea45dcbb44643b39d8bc38
prosperemebo <emeboprosper@gmail.com>
2023-09-01 03:08:00 +0100

    Task 5

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_is_root - This function checks if a node is a leaf
+ * @node: node
+ * Returns: Either 0 or 1
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if(!node)
+		return (0);
+	if(!node->left && !node->right)
+		return (1);
+	return (0);
+}
