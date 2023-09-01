1cf2cfb8345b69051be0b7fb685c9e128ba5288c
prosperemebo <emeboprosper@gmail.com>
2023-09-01 03:14:45 +0100

    Update 5-binary_tree_is_root.c

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -5,13 +5,13 @@
  * binary_tree_is_root - This function checks if a given
  * node is a root
  * @node: node
- * Returns: Either 0 or 1
+ * Return: Either 0 or 1
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if(!node)
+	if (!node)
 		return (0);
-	if(!node->parent)
+	if (!node->parent)
 		return (1);
 	return (0);
 }
