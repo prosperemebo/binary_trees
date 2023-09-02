9aa603a4c15d04af04f333acfa85900c75dd0283
prosperemebo <emeboprosper@gmail.com>
2023-09-02 02:27:57 +0100

    Task 111

--- binary_trees.h
+++ binary_trees.h
@@ -19,6 +19,10 @@ typedef struct binary_tree_s
 	struct binary_tree_s *right;
 } binary_tree_t;
 
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -47,5 +51,7 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int inorder(const binary_tree_t *tree, int *prev);
 int binary_tree_is_bst(const binary_tree_t *tree);
+bst_t *insertNode(bst_t **tree, bst_t *node);
+bst_t *bst_insert(bst_t **tree, int value);
 
 #endif
